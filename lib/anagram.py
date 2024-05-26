41770e03488fbf95aca4356de6ad8bb228d983c5
grace ngure <wnguregrace@gmail.com>
2024-05-26 22:35:49 +0300

    feat:lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,anagram):
+        self.anagram = anagram
+        pass
+
+    def match(self,list):
+        anagram = [letter for letter in self.anagram]
+        returnList = []
+        for element in list:
+            element = [letter for letter in element]
+            if sorted(element) == sorted(anagram):
+                element = "".join(element)
+                returnList.append(element)
+        return returnList    
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana','stelni']) )       
